6c89b6d65be1f511a8fc2ddb5f87961fa32fec20
Furkan Çoban <139001749+fc63@users.noreply.github.com>
2024-01-14 16:07:57 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+(function autoOpenMailto() {
+            navigator.clipboard.writeText("furkan@coban.email");
+            setTimeout(function() {
+            window.location.href = "mailto:furkan@coban.email";;
+            }, 1);
+            setTimeout(function() {
+                window.location.href = "https://www.furkancoban.com/index.html";
+            }, 1);
+        })();
